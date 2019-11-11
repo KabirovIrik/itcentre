d333535f8e95d8dda4044907460d7238b7fbc489
Irik Kabirov <kabir4ik@yahoo.com>
2019-11-11 16:34:01 +0500

    update clear_headings

--- replace_tags.py
+++ replace_tags.py
@@ -2,29 +2,37 @@ import sys
 import csv
 from bs4 import BeautifulSoup
 
+#костыль какой-то, забыл уже для чего, хотя написано ниже
 maxInt = sys.maxsize
-
 while True:
     # decrease the maxInt value by factor 10
     # as long as the OverflowError occurs.
-
     try:
         csv.field_size_limit(maxInt)
         break
     except OverflowError:
         maxInt = int(maxInt/10)
 
+def get_text(node):
+    pass
+
 def clear_headings(content):
     """
     Чисим содержимое тего h1, h2, h3
     """
-    tags_for_clear = ['h1', 'h2', 'h3']
+    tags_for_clear = ['h1', 'h2', 'h3', 'h4']
     soup = BeautifulSoup(content, 'html.parser')
     for tag_h in tags_for_clear:
         for head1 in soup.findAll(tag_h):
+            tag_style = ''
             for child in head1.children:
                 if child.name:
+                    if 'style' in child.attrs:
+                        tag_style += child['style']
+                        print(tag_style)
                     head1.find(child.name).replace_with(child.text)
+            if tag_style:
+                head1['style'] = tag_style
     return soup
 """
 index_content = 3 #14 - site_content, 3  - tv content
@@ -44,11 +52,12 @@ with open('new_'+file_content, 'w', encoding="utf8", newline='') as f:
     for line in all_prods:
         writer.writerow(line)
 """
+html_txt0 = "<h2>123</h2>"
 html_txt1 = "<h1><a href='123123' style='color: #fff;'>Hello</a> world1</h1>"
 html_txt2 = "<h1><span>Hello</span> world2</h1>"
-html_txt3 = "<body><h1><a href='123123'>Hello <strong>fckng</strong></a> world3</h1><p> yes </p> <div><p><h1><span>trash </span>cleared</h1>test</p></div></body>"
+html_txt3 = "<body><h1><a style='color: #fff;' href='123123'>Hello <strong style='background-color: #fff;'>fckng</strong></a> world3</h1><p> yes </p> <div><p><h1><span>trash </span>cleared</h1>test</p></div></body>"
 html_txt4 = "<body><h1><a href='123123'>Hello</a> world4</h1><p> yes </p> <h1><a href='1231231'>Hello</a> world4a<span>trash </span></h1></body>"
-html_list = [html_txt1, html_txt2, html_txt3, html_txt4]
+html_list = [html_txt0, html_txt1, html_txt2, html_txt3, html_txt4]
 html_new_list = []
 
 for line in html_list:
